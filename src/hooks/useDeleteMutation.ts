c793a9e9e24b8d4372e7a9fd3533fd61b9cea12b
winter100 <100taeng@gmail.com>
2025-07-05 18:06:13 +0900

    feat: zod 및 react hook form 세팅

--- src/hooks/useDeleteMutation.ts
+++ src/hooks/useDeleteMutation.ts
@@ -1,9 +1,14 @@
 import { deleteSign } from "@/services/deleteSign";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
-export const useDeleteMutation = () => {
+export const useDeleteMutation = (handleClose: () => void) => {
+  const queryClient = useQueryClient();
   return useMutation({
     mutationKey: ["delete", "sign"],
     mutationFn: deleteSign,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["sign", "list"] });
+      handleClose();
+    },
   });
 };
